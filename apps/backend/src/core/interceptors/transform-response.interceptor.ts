8eb473c845419c929685425b745f803a4e6cb982
tomwray13 <tomwray13@gmail.com>
2023-10-21 20:09:17 +0100

    STARTER UPDATE - transform interceptor when response is null

--- apps/backend/src/core/interceptors/transform-response.interceptor.ts
+++ apps/backend/src/core/interceptors/transform-response.interceptor.ts
@@ -12,6 +12,10 @@ export class TransformResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((response) => {
+        if (!response)
+          return {
+            data: [],
+          };
         if (response.data && response.meta) {
           return {
             data: response.data,
