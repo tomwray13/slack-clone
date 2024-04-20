ebe3c5083c49e8515dcfe4c969e9cc73ba5ea351
Tom Ray <tomwray13@gmail.com>
2024-04-20 12:21:10 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { LoggerService } from './core/logger/logger.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(LoggerService));
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const host = `0.0.0.0`;
+  await app.listen(port, host);
+}
+bootstrap();
